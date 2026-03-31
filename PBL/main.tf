9cb721925cbbe225cbdbea039f633eb5cf726767
Ngumonelson123 <nelsonmbui88@gmail.com>
2026-03-31 09:56:09 +0000

    Update terraform variables && main.tf files

--- PBL/main.tf
+++ PBL/main.tf
@@ -1,30 +1,11 @@
-# Variables
-variable "region" {
-  default = "us-east-1"
-}
-
-variable "vpc_cidr" {
-  default = "172.16.0.0/16"
-}
-
-variable "enable_dns_support" {
-  default = "true"
-}
-
-variable "enable_dns_hostnames" {
-  default = "true"
-}
-
-# Provider
-provider "aws" {
-  region = var.region
-}
-
 # Data source for Availability Zones Dynamically
 data "aws_availability_zones" "available" {
   state = "available"
 }
-
+# Provider
+provider "aws" {
+  region = var.region
+}
 # Create VPC
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
@@ -34,11 +15,11 @@ resource "aws_vpc" "main" {
 
 # Create Public Subnets using Count of Availability Zones
 resource "aws_subnet" "public" {
-  count                    =  length(data.aws_availability_zones.available.names)
-  vpc_id                   = aws_vpc.main.id
-  cidr_block               = cidrsubnet(var.vpc_cidr, 4, 
+  count  = var.preferred_number_of_public_subnets == null ? length(data.aws_availability_zones.available.names) : var.preferred_number_of_public_subnets
+  vpc_id = aws_vpc.main.id
+  cidr_block = cidrsubnet(var.vpc_cidr, 4,
   count.index)
-  map_public_ip_on_launch  = true
-  availability_zone      = data.aws_availability_zones.available.names[count.index]
-  
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+
 }
